8548a0b59bc3a7f61c8e0800941481c9ed502860
gsfortunato <gsfortunato@hotmail.com>
2023-05-23 17:44:54 +0100

    teste automatizado

---
+++ cypress/e2e/cadastro.cy.js
@@ -0,0 +1,33 @@
+///<reference types='cypress'/>
+
+const { faker } = require('@faker-js/faker');
+
+describe('Cadastro', () => {
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    it('Cadastro - sucesso', () => {
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type(faker.internet.password())
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(faker.person.firstName())
+        cy.get('#account_last_name').type(faker.person.lastName())
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso')
+    });
+
+
+
+    // E-mail, Senha, Nome e Sobrenome
+    
+    //var emailfaker = faker.internet.email()
+    //var senhafaker = faker.internet.password()
+    //var nomefaker = faker.person.firstName()
+    //var sobrenomefaker = faker.person.lastName()
+
+    // .type( nome da variavel)
+
+});
---
+++ cypress/e2e/produtos.cy.js
@@ -0,0 +1,29 @@
+///<reference types='cypress'/>
+
+
+describe('Must select a product ', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Select a product', () => {
+        //cy.get('.products > .row').contains('Aero Daily Fitness Tee').click()
+        cy.get('[class="product-block grid"]').eq(2).click()
+    });
+
+    it.only('Adiciona o produto ao carrinho', () => {
+
+        var quantidade = 9
+
+        cy.get('[class="product-block grid"]').contains('Abominable Hoodie').click()
+        cy.get('.button-variable-item-S').click()
+        cy.get('.button-variable-item-Red').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade +' × “Abominable Hoodie” foram adicionados no seu carrinho.')
+    });
+    
+    
+});
