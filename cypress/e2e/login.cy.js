f1343bc7192d06e349759e8a883a90368ee7e18e
gsfortunato <gsfortunato@hotmail.com>
2023-05-23 14:33:58 +0100

    first commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress"/>
+
+context('Funcionalidae login', ()=>{
+
+    it('Login com sucesso', ()=> {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.page-title').should('contain', 'Minha conta')
+    })
+
+
+    it('Usuário inválido', ()=>{
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('contain', 'Erro')
+    })
+
+    it('Senha inválida', ()=>{
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain', 'Erro')
+    })
+
+    
+})
